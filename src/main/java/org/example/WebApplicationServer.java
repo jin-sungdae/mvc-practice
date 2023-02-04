0ef36e02474b4914fe5d8a2fb2f8b64a2c8190a4
sjin <wlstjdeo00@gmail.com>
2023-02-04 17:57:38 +0900

    FEATURE : logback file 추가

--- src/main/java/org/example/WebApplicationServer.java
+++ src/main/java/org/example/WebApplicationServer.java
@@ -1,8 +1,24 @@
 package org.example;
 
+import org.apache.catalina.startup.Tomcat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
 public class WebApplicationServer {
 
-    public static void main(String[] args) {
+    private static final Logger logger = LoggerFactory.getLogger(WebApplicationServer.class);
 
+    public static void main(String[] args) throws Exception {
+        String webappDirLocation = "webapps/";
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        logger.info("configuring app with basedir: {}", new File("./" + webappDirLocation).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();;
     }
 }
