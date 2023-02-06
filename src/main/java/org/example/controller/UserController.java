74637eaea401f9ada3576602c1f993b0ee0f4775
sjin <wlstjdeo00@gmail.com>
2023-02-06 19:28:13 +0900

    FEATURE : beanFactory  생성

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -1,7 +1,20 @@
 package org.example.controller;
 
 import org.example.annotation.Controller;
+import org.example.annotation.Inject;
+import org.example.service.UserService;
 
 @Controller
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Inject
+    private UserService getUserService() {
+        return userService;
+    }
 }
